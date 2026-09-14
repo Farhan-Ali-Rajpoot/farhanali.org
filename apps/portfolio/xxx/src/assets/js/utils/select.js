99bb324cf471a64a69d0a4b2791ab61315bd588c
Farhan-Ali-Rajpoot <your_id+username@users.noreply.github.com>
2026-09-14 13:34:36 +0600

    Quick push

--- apps/portfolio/xxx/src/assets/js/utils/select.js
+++
@@ -1,6 +0,0 @@
-
-
-export function select(q) { return document.querySelector(q) }
-export function selectAll(q) { return document.querySelectorAll(q) }
-export function selectById(q) { return document.getElementById(e) }
-export function selectByClass(q) { return document.getElementsByClassName(e)} 
